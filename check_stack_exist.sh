c36f08ff53a68300b42c832db26413b086ccf10a
EC2 Default User <ec2-user@ip-10-0-0-217.ap-northeast-1.compute.internal>
2021-12-12 04:29:21 +0000

    shell script

---
+++ check_stack_exist.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ `aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE | jq '.StackSummaries[].StackName' | grep 'sampleStack'` ]; then
+  echo "export STACK_EXIST_FLAG='true'" >> $BASH_ENV
+else
+  echo "export STACK_EXIST_FLAG='false'" >> $BASH_ENV
+fi
+echo "${STACK_EXIST_FLAG}"
