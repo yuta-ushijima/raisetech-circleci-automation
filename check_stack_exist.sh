f8f4f4a7ca67b1302f0a8d5b6b5d4dee04cf6716
EC2 Default User <ec2-user@ip-10-0-0-217.ap-northeast-1.compute.internal>
2021-12-13 13:17:30 +0000

    fix

--- check_stack_exist.sh
+++ check_stack_exist.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ `aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE | jq '.StackSummaries[].StackName' | grep 'sampleStack'` ]; then
-  stack_check: true
+  stack_check = true
 else
-  stack_check: false
+  stack_check = false
 fi
 echo stack_check
