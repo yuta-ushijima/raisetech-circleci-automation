5c9e032bb9657d5387f35feb8d132e25064e09fb
EC2 Default User <ec2-user@ip-10-0-0-217.ap-northeast-1.compute.internal>
2021-12-13 14:08:35 +0000

    fix

--- check_stack_finished.sh
+++ check_stack_finished.sh
@@ -1,4 +1,7 @@
 aws cloudformation update-stack --stack-name sampleStack --region ap-northeast-1 --template-body file://sample_stack.yml
 RESULT="$?"
-if [[ "$RESULT" = "254" ]]; then exit 0
+if [ "$RESULT" = "254" ]; then
+  exit 0
+else
+  aws cloudformation wait stack-update-complete --stack-name sampleStack
 fi
